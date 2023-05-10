da5a87006efbbb314bf4488a1001f4d32a38f001
Your Name <ss.pena@uniandes.edu.co>
2023-05-10 18:31:41 -0500

    f

---
+++ App/Untitled-1.py
@@ -0,0 +1,18 @@
+"prueba"
+
+n =10.23989438985
+o =-43.09343095
+def redondear_4_hacia_arriba(num):
+    parte_decimal = str(num).split('.')[1]
+    parte_entera =str(num).split('.')[0]
+    cuatro_decimales = parte_decimal[0:4]
+    num=float(parte_entera+'.'+cuatro_decimales)
+    if num>0:
+        num=num+0.0001
+    return num
+
+e = redondear_4_hacia_arriba(n)
+f=redondear_4_hacia_arriba(o)
+print(e)
+print(f)
+

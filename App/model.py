537826be2d924c72ec6b1f0f9447bf8a17270980
Your Name <ss.pena@uniandes.edu.co>
2023-05-30 15:50:04 -0500

    h

--- App/model.py
+++ App/model.py
@@ -853,6 +853,8 @@ def req_4(data_structs,lat_1,long_1,lat_2,long_2):
     ###recorridos minimos del nodo de incio a todos los demás
     recorridos_inicio=bf.BellmanFord(grafo,nodo_inicio)
 
+    if bf.hasPathTo(recorridos_inicio,nodo_fin)==False:
+        return distancia_entre_punto_inicio_nodo,distancia_entre_punto_fin_nodo,nodo_inicio,nodo_fin
     recorrido_min=bf.pathToArray(recorridos_inicio,nodo_fin)
 
     
